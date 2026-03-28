77968f940b482f1c607ad1d40838641d77f13b81
Anatoly <naumovtoliasik92@gmail.com>
2026-03-28 16:32:30 +0500

    project structur

---
+++ tests/pages(models)/MainPage.ts
@@ -0,0 +1,8 @@
+import { Page } from '@playwright/test'
+import { BasePage } from './BasePage';
+export class MainPage extends BasePage {
+    async open() {
+        this.page.goto('https://rutube.ru/');
+    }
+}
+
