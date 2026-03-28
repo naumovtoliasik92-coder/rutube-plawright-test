3fce3b6d3eaa6216742c920f606cafb995afa848
Anatoly <naumovtoliasik92@gmail.com>
2026-03-28 17:48:01 +0500

    add fixtures

---
+++ tests/fixtures/fixtures.ts
@@ -0,0 +1,27 @@
+import { test as base } from '@playwright/test';
+import { MainPage } from '../pages(models)/MainPage';
+
+
+// Declare the types of your fixtures.
+type MyFixtures = {
+    mainPage: MainPage
+};
+
+// Extend base test by providing "todoPage" and "settingsPage".
+// This new "test" can be used in multiple test files, and each of them will get the fixtures.
+export const test = base.extend<MyFixtures>({
+  mainPage: async ({ page }, use) => {
+    // Set up the fixture.
+    const mainPage = new MainPage(page);
+    await mainPage.open();
+    await mainPage.closeModal()
+
+    // Use the fixture value in the test.
+    await use(mainPage);
+  },
+
+  settingsPage: async ({ page }, use) => {
+    await use(new SettingsPage(page));
+  },
+});
+export { expect } from '@playwright/test';
--- tests/specs(tests)/unauthorizeit/mainPage.spec.ts
+++ tests/specs(tests)/unauthorizeit/mainPage.spec.ts
@@ -1,28 +1,14 @@
-import test from "@playwright/test";
+import {test, expect} from '../../fixtures/fixtures'
 import { MainPage } from "../../pages(models)/MainPage";
 
-test('Открытие главной страницы', async ({ page }) => {
-    const mainPage = new MainPage(page);
-    await mainPage.open();
-})
-
-test('Проверка доступности элементов хедера', async ({ page }) => {
-    const mainPage = new MainPage(page);
-    await mainPage.open();
-      await mainPage.closeModal();
+test('Проверка доступности элементов хедера', async ({ mainPage }) => {
     await mainPage.headerHasCorrectAriaSnapshot();
 })
 
-test('Проверка доступности табов категорий', async ({ page }) => {
-    const mainPage = new MainPage(page);
-    await mainPage.open();
-    await mainPage.closeModal();
+test('Проверка доступности табов категорий', async ({ mainPage }) => {
     await mainPage.categoriesTabsHasCorrectAriaSnapshot();
 })
 
-test('Проверка доступности элементов меню', async ({ page }) => {
-    const mainPage = new MainPage(page);
-    await mainPage.open();
-    await mainPage.closeModal();
+test('Проверка доступности элементов меню', async ({ mainPage }) => {
     await mainPage.menuHasCorrectAriaSnapshot();
 })
